81f93a2f7a1b58ba29aef8d3fde5639ec353a72e
AzrilYusuf <muhammadazrilyusuf.w3rk@gmail.com>
2025-09-24 21:23:31 +0700

    feat/refactor(shopping_screen): Create shopping screen with product input and calculation logic (Move shopping_screen into its own file).

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // MaterialApp is a widget that introduces a number of widgets (is the main wrapper)
-    return MaterialApp(
-      // home is referred to tha main page (the first screen that will be displayed)
-      home: ProfileScreen(),
-    );
-  }
-}
-
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
